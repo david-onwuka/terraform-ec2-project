bc33966191ac728964d737b4ed8fe14ec86087f0
David Onwuka <davidonwuka21@gmail.com>
2026-04-05 09:16:31 +0100

    Terraform EC2 project with nginx (Clean setup)

---
+++ main.tf
@@ -0,0 +1,52 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "web_sg" {
+  name = "web-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+  key_name = "terraform-key1"
+
+  security_groups = [aws_security_group.web_sg.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              service docker start
+              docker run -d -p 80:80 nginx
+              EOF
+
+  tags = {
+    Name = "terraform-devops-web"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
